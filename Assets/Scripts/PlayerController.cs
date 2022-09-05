e8c5455881eec1901eb7a2e4a9dac3147a6fbadc
Chris Porter <46310006+bsxp@users.noreply.github.com>
2022-09-05 00:42:35 -0700

    Player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,16 @@ public class PlayerController : MonoBehaviour
 
 	public float moveSpeed;
 
+	private Animator anim;
+
+	private bool playerMoving;
+
+	private Vector2 lastMove;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,14 +25,26 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
 		float vertical = Input.GetAxisRaw("Vertical");
 
-		if (horizontal > 0.5f || horizontal < 0.5f) // Player is pressing right or left, respectively
+		playerMoving = false;
+
+		if (horizontal > 0.5f || horizontal < -0.5f) // Player is pressing right or left, respectively
 		{
 			transform.Translate(new Vector3(horizontal * moveSpeed * Time.deltaTime, 0f, 0f));
+			playerMoving = true;
+			lastMove = new Vector2(horizontal, 0f);
 		}
 
-		if (vertical > 0.5f || vertical < 0.5f) // Player is pressing right or left, respectively
+		if (vertical > 0.5f || vertical < -0.5f) // Player is pressing right or left, respectively
 		{
 			transform.Translate(new Vector3(0f, vertical * moveSpeed * Time.deltaTime, 0f));
+			playerMoving = true;
+			lastMove = new Vector2(0f, vertical);
 		}
+
+		anim.SetFloat("MoveX", horizontal);
+		anim.SetFloat("MoveY", vertical);
+		anim.SetBool("PlayerMoving", playerMoving);
+		anim.SetFloat("LastMoveX", lastMove.x);
+		anim.SetFloat("LastMoveY", lastMove.y);
     }
 }
